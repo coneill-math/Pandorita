c6685644f45fe38821814d90a3a49c774ab91679
Christopher ONeill <musicman3320@gmail.com>
2012-05-21 11:32:54 -0700

    Testing php support.

---
+++ temp.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Pandorita by musicman3320</title>
+
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <h1>Pandorita</h1>
+        <p>Cocoa-based pandora.com player using pianolib</p>
+        <p class="view"><a href="https://github.com/musicman3320/Pandorita">View the Project on GitHub <small>musicman3320/Pandorita</small></a></p>
+<!--
+        <ul>
+          <li><a href="https://github.com/musicman3320/Pandorita/zipball/master">Download <strong>ZIP File</strong></a></li>
+          <li><a href="https://github.com/musicman3320/Pandorita/tarball/master">Download <strong>TAR Ball</strong></a></li>
+          <li><a href="https://github.com/musicman3320/Pandorita">View On <strong>GitHub</strong></a></li>
+        </ul>
+-->
+      </header>
+      <section>
+        <h3>Welcome to Pandorita's GitHub Page</h3>
+
+<p>Pandorita is a native Mac OS X client for the internet radio service Pandora.  It is written entirely in Cocoa, and built to be lightweight, easy to use, and fully functional.  Internally it uses the amazing <a href="https://github.com/PromyLOPh/pianobar">pianobar</a> library to communicate with Pandora's server.  </p>
+
+<h3>So what can it do?</h3>
+
+<p>Pandorita is still in its very early stages, so many features are not yet implemented and some features may have bugs.  That said, nearly everything you would want to do with Pandora can already be done in Pandorita.  Currently, you can use Pandorita to: </p>
+
+<ul>
+<li>Stream tracks from any station.<br>
+</li>
+<li>Rate tracks, including ones which have already finished playing.<br>
+</li>
+<li>View album artwork for the current song.<br>
+</li>
+<li>Create stations by searching for artists and songs, as well as rename or delete existing stations.<br>
+</li>
+<li>Enable Growl notifications.
+</li>
+<li>Enable global hotkeys.
+</li>
+</ul>
+
+<p>Last.fm scrobbling is soon to come as well.  <?php echo "This is from php" ?></p>
+
+<h3>Cool!  Where can I get it?</h3>
+
+<p>You can download the latest version (0.2.3) <a href="http://musicman3320.github.com/Pandorita/releases/0.2.3/Pandorita-0.2.3.tar.gz">here</a>.  If you are feeling adventurous, you can download and build the absolute latest source from the Github repository, but be warned, this is likely to contain bugs.</p>
+
+<h3>Support or Contact</h3>
+
+<p>Having trouble with Pandorita?  Found a bug?  Got a feature you would like to see implemented?  I would love to hear from you!  Send an email to musicman dot pandorita at gmail dot com.</p>
+      </section>
+      <footer>
+        <p>This project is maintained by <a href="https://github.com/musicman3320">musicman3320</a></p>
+        <p><small>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>
+      </footer>
+    </div>
+    <script src="javascripts/scale.fix.js"></script>
+    
+  </body>
+</html>
